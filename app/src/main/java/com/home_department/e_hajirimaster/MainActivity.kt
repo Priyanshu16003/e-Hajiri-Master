90ad377b1238da0e788053178252f4d49cc794e8
Priyanshu <pteof19@gmail.com>
2022-10-05 23:22:36 +0530

    Initial commit

---
+++ app/src/main/java/com/home_department/e_hajirimaster/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.home_department.e_hajirimaster
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.home_department.e_hajirimaster.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        Thread.sleep(1000)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        val navController = findNavController(R.id.fragment)
+        binding.bottomNavView.setupWithNavController(navController)
+
+    }
+}
---
+++ app/src/main/java/com/home_department/e_hajirimaster/fragments/UpcomingDuties.kt
@@ -0,0 +1,12 @@
+package com.home_department.e_hajirimaster.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.home_department.e_hajirimaster.R
+
+class UpcomingDuties : Fragment(R.layout.upcoming_duties) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
