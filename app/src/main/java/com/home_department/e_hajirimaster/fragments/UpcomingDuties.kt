bbb8cbb59aced2799de31ded1555189b68726be1
Priyanshu <priyanshutandel7@gmail.com>
2022-10-07 19:01:53 +0530

    second evolution round round commit

--- app/src/main/java/com/home_department/e_hajirimaster/fragments/UpcomingDuties.kt
+++ app/src/main/java/com/home_department/e_hajirimaster/fragments/UpcomingDuties.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.home_department.e_hajirimaster.R
+import com.home_department.e_hajirimaster.databinding.ActivityMainBinding
 
 class UpcomingDuties : Fragment(R.layout.upcoming_duties) {
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
     }
+
 }
