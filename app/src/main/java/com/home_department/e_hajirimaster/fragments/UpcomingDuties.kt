375c05b5d666f47541696863665c1b188f5551ba
Priyanshu <priyanshutandel7@gmail.com>
2022-10-08 03:24:47 +0530

    finally some progress

--- app/src/main/java/com/home_department/e_hajirimaster/fragments/UpcomingDuties.kt
+++ app/src/main/java/com/home_department/e_hajirimaster/fragments/UpcomingDuties.kt
@@ -1,16 +1,35 @@
 package com.home_department.e_hajirimaster.fragments
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.home_department.e_hajirimaster.R
 import com.home_department.e_hajirimaster.databinding.ActivityMainBinding
+import com.home_department.e_hajirimaster.databinding.UpcomingDutiesBinding
+
 
 class UpcomingDuties : Fragment(R.layout.upcoming_duties) {
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+
+lateinit var binding: UpcomingDutiesBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
 
     }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        return inflater.inflate(R.layout.upcoming_duties, container, false)
+    }
+
+
 
 }
