8d5b8af67d13daf159221a7fac50c75ed213f05e
Priyanshu <priyanshutandel7@gmail.com>
2022-10-08 03:02:27 +0530

    finally some progress

---
+++ app/src/main/java/com/home_department/e_hajirimaster/LoginActivity.kt
@@ -0,0 +1,51 @@
+package com.home_department.e_hajirimaster
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.home_department.e_hajirimaster.databinding.ActivityLoginBinding
+import com.home_department.e_hajirimaster.databinding.ActivityMainBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private  lateinit var firebaseAuth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.button.setOnClickListener{
+            val email = binding.email.text.toString()
+            val pass = binding.password.text.toString()
+
+            if(email.isNotEmpty() && pass.isNotEmpty()){
+                firebaseAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener{
+                    if(it.isSuccessful){
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+                    else{
+                        Toast.makeText(this,"Wrong UserName or Password",Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if(firebaseAuth.currentUser != null){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
